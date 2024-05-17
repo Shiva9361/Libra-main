3aa80740a9fab413f4fbae76337e4c981e7d6820
Shiva9361 <shivadharshansankar936@gmail.com>
2024-05-17 21:50:12 +0530

    uncommented daily remainder

--- jobs.py
+++ jobs.py
@@ -7,16 +7,15 @@ PASSWORD = password
 
 
 def send_daily_reminder(email, username):
-    print("hi")
-    # msg = EmailMessage()
-    # msg["Subject"] = "Login Reminder"
-    # msg["From"] = SENDER
-    # msg["To"] = email
-    # body = "This is your reminder to visit Libra !! \nSo many books waiting to be read\n\n\nThis is a auto generated text, Please Don't Reply"
-    # msg.set_content(body)
-    # with smtplib.SMTP_SSL("smtp.gmail.com") as smtp:
-    #     smtp.login(SENDER, PASSWORD)
-    #     smtp.send_message(msg)
+    msg = EmailMessage()
+    msg["Subject"] = "Login Reminder"
+    msg["From"] = SENDER
+    msg["To"] = email
+    body = "This is your reminder to visit Libra !! \nSo many books waiting to be read\n\n\nThis is a auto generated text, Please Don't Reply"
+    msg.set_content(body)
+    with smtplib.SMTP_SSL("smtp.gmail.com") as smtp:
+        smtp.login(SENDER, PASSWORD)
+        smtp.send_message(msg)
 
 
 send_daily_reminder("shivadharshansankar936@gmail.com", "gg")
