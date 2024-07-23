31d925376ca2068b4c7d1efe899b029d2901de5c
Shiva9361 <shivadharshansankar936@gmail.com>
2024-07-23 13:30:45 +0530

    feat: divisoin to submodules

--- run_library.sh
+++ run_library.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gnome-terminal -- bash -c "cd frontend/Libra-v2;npm run dev;"
-gnome-terminal -- bash -c "cd backend/; python3 app.py"
-gnome-terminal -- bash -c "cd backend;celery -A app.celery beat --loglevel=info"
-gnome-terminal -- bash -c "cd backend;celery -A app.celery worker -l info -E"
+gnome-terminal -- bash -c "cd libra-vue-frontend;npm run dev;"
+gnome-terminal -- bash -c "cd libra-flask-backend/; python3 app.py"
+gnome-terminal -- bash -c "cd libra-flask-backend/;celery -A app.celery beat --loglevel=info"
+gnome-terminal -- bash -c "cd libra-flask-backend/;celery -A app.celery worker -l info -E"
